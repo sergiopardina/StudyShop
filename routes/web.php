7a93c0ac3a214a49c94c3f119c06e30855a16193
eger001 <mainforphp@gmail.com>
2023-02-26 00:26:56 +0200

    admin route add/ admin.blade init/ admin_layout.blade init/ AuthController created & login() init/

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [AuthController::class, 'login'])->name('admin');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
