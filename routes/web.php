058b5c4c27aef016ecdb9e0f2f04aa3011caac4d
eger001 <mainforphp@gmail.com>
2023-02-27 01:07:08 +0200

    Route group for locale add locale route init

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,34 @@ use App\Http\Controllers\AdminController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::group(['middleware' => ['setLocale']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/admin', [AuthController::class, 'login'])->name('admin');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::get('/admin/index', [AdminController::class, 'index'])->name('admins.index');
+
+    Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admins.destroy');
+
+    Route::get('/admin/edit/{admin}', [AdminController::class, 'edit'])->name('admins.edit');
+
+    Route::put('/admins/{admin}', [AdminController::class, 'update'])->name('admins.update');
 });
+Route::post('/locale', [App\Http\Controllers\LocaleController::class, 'update'])->name('locale.update');
 
-Route::get('/admin', [AuthController::class, 'login'])->name('admin');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/admin/index', [AdminController::class, 'index'])->name('admins.index');
-
-Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admins.destroy');
-
-Route::get('/admin/edit/{admin}', [AdminController::class, 'edit'])->name('admins.edit');
-
-Route::put('/admins/{admin}', [AdminController::class, 'update'])->name('admins.update');
 
 
 require __DIR__.'/auth.php';
