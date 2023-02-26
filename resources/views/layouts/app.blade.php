6529e8804ce8aca547a0bab296541cb9c45e2046
eger001 <mainforphp@gmail.com>
2023-02-26 05:05:04 +0200

    app.blade some libraries add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,14 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" type="image/x-image" href="{{ asset('images/icon.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="/css/user.css">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
