e101240f9fa8069f88a928fe286f401069745f6b
vladislav <vladislav.nikitin.home@gmail.com>
2023-02-26 18:59:49 +0200

    registered user changes

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'admin' => false,
         ]);
 
         event(new Registered($user));
