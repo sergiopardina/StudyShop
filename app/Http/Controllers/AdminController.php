82f8f991f64cffa6760a9f3e22e7f12c6d9e36dd
eger001 <mainforphp@gmail.com>
2023-02-27 00:13:06 +0200

    todo added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -33,6 +33,8 @@ class AdminController extends Controller
     {
         $admin = Admin::findOrFail($id);
 
+
+        //TODO validation
 //        $request->validate([
 //            'name' => 'required',
 //            'email' => 'required|email' . $admin->id,
