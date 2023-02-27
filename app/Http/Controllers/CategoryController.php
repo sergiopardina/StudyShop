e6ac768e47c3e50e6603b301ed2593df89eae447
Serkhio <pardinaserkhio@gmail.com>
2023-02-28 00:53:20 +0200

    categories init, route

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = Category::all();
+        return view('welcome', [
+            'categories'=>$categories,
+        ]);
+    }
+
+}
