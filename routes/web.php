0826da673d7bf916edea73dd099f7cffbeca4c78
vladislav <vladislav.nikitin.home@gmail.com>
2023-03-01 21:25:14 +0200

    web.php use CategoryController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
