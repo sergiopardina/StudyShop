50d8294043b3fae66d8d46c51ef39713db35228d
eger001 <mainforphp@gmail.com>
2023-03-03 01:30:26 +0200

    look changed

--- routes/web.php
+++ routes/web.php
@@ -18,42 +18,50 @@ use App\Http\Controllers\BrandController;
 |
 */
 Route::group(['middleware' => ['setLocale']], function () {
+
     Route::get('/', function () {
         return view('welcome');
     });
 
-    Route::get('/admin', [AuthController::class, 'login'])->name('admin');
+    Route::get('/admin', [AuthController::class, 'login'])
+        ->name('admin');
 
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
+    })->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/', [AuthController::class, 'checkChangePass'])->name('change.password');
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware('auth')
+        ->group(function () {
+//        Route::get('/', [AuthController::class, 'checkChangePass'])->name('change.password');
+        Route::get('/profile', [ProfileController::class, 'edit'])
+            ->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
     });
 
     Route::get('/catalog', function () {
     return view('catalog');
     });
 
-    Route::get('/admin/index', [AdminController::class, 'index'])->name('admins.index');
-
-    Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admins.destroy');
-
-    Route::get('/admin/edit/{admin}', [AdminController::class, 'edit'])->name('admins.edit');
-
-    Route::put('/admins/{admin}', [AdminController::class, 'update'])->name('admins.update');
-    Route::get('/admins/add', [AdminController::class, 'add'])->name('admins.add');
-
-    Route::post('/admins/index', [AdminController::class, 'store'])->name('admins.create');
+    Route::get('/admin/index', [AdminController::class, 'index'])
+        ->name('admins.index');
+    Route::delete('/admin/{id}', [AdminController::class, 'destroy'])
+        ->name('admins.destroy');
+    Route::get('/admin/edit/{admin}', [AdminController::class, 'edit'])
+        ->name('admins.edit');
+    Route::put('/admins/{admin}', [AdminController::class, 'update'])
+        ->name('admins.update');
+    Route::get('/admins/add', [AdminController::class, 'add'])
+        ->name('admins.add');
+    Route::post('/admins/index', [AdminController::class, 'store'])
+        ->name('admins.create');
 
     Route::get('/about', function () {
         return view('about');
     });
-
     Route::get('/contacts', function () {
         return view('contacts');
     });
@@ -61,20 +69,33 @@ Route::group(['middleware' => ['setLocale']], function () {
         return view('account');
     });
 
-    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
-    Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::put('/category/{category}', [CategoryController::class, 'update'])->name('category.update');
-    Route::delete('/category/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::get('/category', [CategoryController::class, 'index'])
+        ->name('category.index');
+    Route::get('/category/create', [CategoryController::class, 'create'])
+        ->name('category.create');
+    Route::post('/category/store', [CategoryController::class, 'store'])
+        ->name('category.store');
+    Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])
+        ->name('category.edit');
+    Route::put('/category/{category}', [CategoryController::class, 'update'])
+        ->name('category.update');
+    Route::delete('/category/{category}', [CategoryController::class, 'destroy'])
+        ->name('category.destroy');
 
-    Route::get('/brand', [BrandController::class, 'index'])->name('brand.index');
-    Route::get('/brand/create', [BrandController::class, 'create'])->name('brand.create');
-    Route::post('/brand/store', [BrandController::class, 'store'])->name('brand.store');
-    Route::get('/brand/edit/{brand}', [BrandController::class, 'edit'])->name('brand.edit');
-    Route::put('/brand/{brand}', [BrandController::class, 'update'])->name('brand.update');
-    Route::delete('/brand/{brand}', [BrandController::class, 'destroy'])->name('brand.destroy');
+    Route::get('/brand', [BrandController::class, 'index'])
+        ->name('brand.index');
+    Route::get('/brand/create', [BrandController::class, 'create'])
+        ->name('brand.create');
+    Route::post('/brand/store', [BrandController::class, 'store'])
+        ->name('brand.store');
+    Route::get('/brand/edit/{brand}', [BrandController::class, 'edit'])
+        ->name('brand.edit');
+    Route::put('/brand/{brand}', [BrandController::class, 'update'])
+        ->name('brand.update');
+    Route::delete('/brand/{brand}', [BrandController::class, 'destroy'])
+        ->name('brand.destroy');
 });
-Route::post('/locale', [App\Http\Controllers\LocaleController::class, 'update'])->name('locale.update');
+Route::post('/locale', [App\Http\Controllers\LocaleController::class, 'update'])
+    ->name('locale.update');
 
 require __DIR__.'/auth.php';
