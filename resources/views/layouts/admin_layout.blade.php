d8079351ceb4bfd697063f7b9eb530f62ec0d81d
eger001 <mainforphp@gmail.com>
2023-03-03 01:11:20 +0200

    add admins.add route

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -88,7 +88,7 @@
                                 <div class="dropdown-admins-menu drop-down">
                                     <ul>
                                         <li><a href={{route('admins.index')}}><i class="bi bi-person-rolodex"></i>{{__('All personal')}}</a></li>
-                                        <li><a href=""><i class="bi bi-person-plus-fill"></i>{{__('Add new admin')}}</a></li>
+                                        <li><a href="{{route('admins.add')}}"><i class="bi bi-person-plus-fill"></i>{{__('Add new admin')}}</a></li>
                                         <li><a href=""><i class="bi bi-person-check-fill"></i>{{__('Assign roles')}}</a></li>
                                     </ul>
                                 </div>
