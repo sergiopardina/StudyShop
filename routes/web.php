98f9799eab066fe458e6d02fd7ce7480859f2930
eger001 <mainforphp@gmail.com>
2023-03-12 03:34:06 +0200

    name to route added

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['middleware' => ['setLocale']], function () {
 
     Route::get('/', function () {
         return view('welcome');
-    });
+    })->name('welcome');
 
     Route::get('/dashboard', function () {
         return view('dashboard');
