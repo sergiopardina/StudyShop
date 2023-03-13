eb608184e0e3185fc56131aacb7908fbb570413b
eger001 <mainforphp@gmail.com>
2023-03-13 02:20:29 +0200

    auth middleware delete in __construct

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Auth;
 class CategoryController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
 
     /**
      * Display a listing of the resource.
